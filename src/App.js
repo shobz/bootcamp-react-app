d04392c7eab6c817e3fd54c0bc0354600229355b
Shahab uddin <shobz@live.com>
2020-09-21 02:19:49 +0500

    class 4
    made changes for class 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Dinner from './dinner';
+// import Dinner from './dinner';
+import {Message} from './message';
 
 function App() {
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false);
+  
   return (
-    <div className="App"> 
+    <div className={`App ${isMorning ? 'dayLight' : ''}`}> 
+    <h3>Day Time = {isMorning ? 'Morning' : 'Night'}</h3>
+    <Message counter={count} />
+    {/* <h2>this is class 4. lets count {count} </h2> */}
     {/* <Dinner></Dinner> */}
-    <Dinner dishName="nihari" sweetDish="Kheer" />
+    {/* <Dinner dishName="nihari" sweetDish="Kheer" /> */}
+<hr />
+    <button onClick={() => setCount(count + 1)}>
+        Update Counter</button>
+
+    <button onClick={() => setMorning(!isMorning)}>
+      Update Day
+    </button>
+
     </div>
   );
 }
 
+ 
 export default App;
---
+++ src/message.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Message(props)
+    {
+        return(
+        <h3>Counter value is {props.counter}</h3>
+        )
+    }
